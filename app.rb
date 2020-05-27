47c5a9e90e506e8f0ca28c0f6f021682b90368b3
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-27 11:18:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,9 @@ class App < Sinatra::Base
   end
   
   post '/puppy' do
-    @puppy_data = params[:name][:breed][:age]
-    @new_puppy = Puppy.new(@puppy_data)
-    
+   # @puppy_data = params[:name][:breed][:age]
+   # @new_puppy = Puppy.new(@puppy_data)
+    puts params
     erb :display_puppy
     binding.pry
   end
